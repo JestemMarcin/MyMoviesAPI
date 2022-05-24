40c467a28eda445c0cc158135e889ee1b98cf438
o120d6@gmail.com <o120d6@gmail.com>
2022-05-24 18:16:45 +0200

    added null for year of prod

--- MyMovies/Models/Movie.cs
+++ MyMovies/Models/Movie.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int ProductionYear { get; set; }
+        public int? ProductionYear { get; set; }
     }
 }
